65bdc18903481c061731bc1b9f963b694d67e642
Alan C <alanc@kidney.org>
2017-08-30 15:34:26 -0400

    Added button function for displaying number on calculator

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,4 +1,26 @@
-Memory = "0";
-Current = "0";
-Operation = "0";
-MAXLENGTH = "22";
+Memory = "0";       // to initialize a memory variable
+Current = 0;      // Current Value of Display
+Operation = 0;    // +, -, *, / 
+MAXLENGTH = 22;   // maximum length of digits before decimal. Will be adjusted.
+
+function AddDigit(dig)  {//This function will add digits to the display.
+if (Current.length > MAXLENGTH) 
+    {
+    Current = "Number is too long!!!!"; //we've reached length limit
+    }
+    else
+    {
+        if ( (Current === 0) && (Current % 1 === 0) )  //Don't need Eval statement Gives Warning - Checking for Decimil point with Modus.
+        { 
+            Current = dig;
+            
+        }
+        else
+        { 
+            Current = Current + dig;
+        }
+    }
+    document.getElementById("Display").value = Current;
+    }
+
+
