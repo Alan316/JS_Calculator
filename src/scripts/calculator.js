c746cc8e73d7e1893b2945d35d302c43503fe50a
Alan C <alanc@kidney.org>
2017-09-04 22:12:46 -0400

    Added functions for Clear and All Clear buttons, removed default display value of 0, and changed current display variable name

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,81 +1,93 @@
 Memory = "0";       // to initialize a memory variable
-Current = 0;      // Current Value of Display
+CurrentDisp = 0;      // CurrentDisp Value of Display
 Operation = 0;    // +, -, *, / 
 MAXLENGTH = 17;   // maximum length of digits before decimal. Will be adjusted.
 
 function AddDigit(dig)  {//This function will add digits to the display.
-if (Current.length > MAXLENGTH) 
+if (CurrentDisp.length > MAXLENGTH) 
     {
-    Current = "ERROR! Number is too long!"; //we've reached length limit
+    CurrentDisp = "ERROR! Number is too long!"; //we've reached length limit
     }
     else
     {
-        if ( (Current === 0) && (Current % 1 === 0) )  //Don't need Eval statement Gives Warning - Checking for Decimil point with Modus.
+        if ( (CurrentDisp === 0) && (CurrentDisp % 1 === 0) )  //Don't need Eval statement Gives Warning - Checking for Decimil point with Modus.
         { 
-            Current = dig;
+            CurrentDisp = dig;
             
         }
         else
         { 
-            Current = Current + dig;
+            CurrentDisp = CurrentDisp + dig;
         }
     }
-    document.getElementById("Display").value = Current;
+    document.getElementById("Display").value = CurrentDisp;
     }
 
 
 function decPoint()
 {
-    if (Current.length === 0)
+    if (CurrentDisp.length === 0)
         {
-            Current = "0.";
+            CurrentDisp = "0.";
         }
     
     else 
         {
-    if (Current.indexOf(".") == -1)  //checks to see if a decimal point already exists in the number
+    if (CurrentDisp.indexOf(".") == -1)  //checks to see if a decimal point already exists in the number
         {
-            Current =  Current + ".";
+            CurrentDisp =  CurrentDisp + ".";
         }
-    document.getElementById("Display").value = Current; 
+    document.getElementById("Display").value = CurrentDisp; 
 }
 }
 
 
 function exponent()
 {
-if (Current.indexOf("e") == -1 )
+if (CurrentDisp.indexOf("e") == -1 )
     {
-    Current = Current + "e0";
-    document.getElementById("Display").value = Current; 
+    CurrentDisp = CurrentDisp + "e0";
+    document.getElementById("Display").value = CurrentDisp; 
     }
 }
 
 
 function PlusMinus()
  {
-  if  (Current.indexOf("e") != -1)  //Checking for the exponent
+  if  (CurrentDisp.indexOf("e") != -1)  //Checking for the exponent
     { 
-        var epos = Current.indexOf("e-");
+        var epos = CurrentDisp.indexOf("e-");
         if (epos != -1)
          {
-            Current = Current.substring(0,1+epos) + Current.substring(2+epos);
+            CurrentDisp = CurrentDisp.substring(0,1+epos) + CurrentDisp.substring(2+epos);
          } 
             else
          { 
-            epos = Current.indexOf("e");
-            Current = Current.substring(0,1+epos) + "-" + Current.substring(1+epos);
+            epos = CurrentDisp.indexOf("e");
+            CurrentDisp = CurrentDisp.substring(0,1+epos) + "-" + CurrentDisp.substring(1+epos);
          }
     } else                          //no ecponent           
     {  
-        if ( Current.indexOf("-") === 0 )
+        if ( CurrentDisp.indexOf("-") === 0 )
          { 
-            Current = Current.substring(1);
+            CurrentDisp = CurrentDisp.substring(1);
          } 
             else
          { 
-            Current = "-" + Current;
+            CurrentDisp = "-" + CurrentDisp;
          }
     }
-    document.getElementById("Display").value = Current; 
+    document.getElementById("Display").value = CurrentDisp; 
+ }
+
+function ClearDisp()        //Clear Current Entry
+ { CurrentDisp = "";
+ document.getElementById("Display").value = CurrentDisp;
+ }
+
+function AllClear()         //Clear all entries including operation and memory
+ { CurrentDisp = "";
+   Operation = 0;            
+   Memory = "0";                 
+   document.getElementById("Display").value = CurrentDisp;
  }
